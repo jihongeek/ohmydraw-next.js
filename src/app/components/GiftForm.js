271fc2b692b6bd1dc6ddf27a90155417f4f8e97b
jihongeek <jihongeek@kakao.com>
2025-01-23 16:54:54 +0900

    Chore: 경품 설정 컴포넌트, 폼 상태 구조 및 처리 로직 변경

--- src/app/components/GiftForm.js
+++ src/app/components/GiftForm.js
@@ -1,60 +1,65 @@
-import './GiftForm.css'
-import ErrorMessage from './ui/ErrorMessage'
-import FileUpload from './FileUpload'
-import TextInput from './ui/TextInput'
-const GiftForm = ({giftDataArray,errorData,setGiftData,nowIndex}) => {
-    const errorMessages = {
-        "name_wrong_character" : "경품명은 특수문자를 제외한 영한문자 그리고 숫자만 가능합니다.", 
-        "no_file" : "파일을 업로드 해주세요",
-        "invalid_file" : "1mb 이하의 jpg, png 파일만 업로드 가능합니다."
-    }
-    const onGiftFileChange = (e) => {
-        setGiftData(giftDataArray.map((giftData,index)=>{
-            if (index === nowIndex && e.target.files.length > 0)
-                return {...giftData, giftFile : e.target.files[0]}
-            return giftData
-        }))
-    }
-    const onGiftNameChange = (e) => {
-        setGiftData(giftDataArray.map( (giftData,index) => {
-            if (index === nowIndex)
-                return {...giftData, giftName : e.target.value}
-            return giftData 
-        }))
-    }
-    return (
-        <div className = "GiftForm">
-            <div className="form_start">
-                <p className = "form_label gift_name">경품 이름</p>
-                <p className = "form_indicator">경품 {nowIndex+1}</p>
-            </div>
-            <TextInput 
-                type = {'gift_name'} 
-                defaultValue={giftDataArray[nowIndex].giftName}
-                onChange={onGiftNameChange}      
-                hasError={errorData.nameErrorStatus !== "none"}
-            />
-            <ErrorMessage
-                type={'gift_name'}
-                errorType={errorData.nameErrorStatus}
-                errorMessages={errorMessages}
-            />
-            <p className = "form_label gift_file" >경품 파일</p>
-            <FileUpload 
-                fileIndex={nowIndex}
-                fileName={giftDataArray[nowIndex].giftFile.name}
-                hasError={errorData.fileErrorStatus !== 'none'}
-                onGiftFileChange={onGiftFileChange}
-                accept={'image/png, image/jpeg'}
-            />
+import './GiftForm.css';
+import ErrorMessage from './ui/ErrorMessage';
+import FileUpload from './FileUpload';
+import TextInput from './ui/TextInput';
 
-            <ErrorMessage
-                type={'gift_file'}
-                errorType={errorData.fileErrorStatus}
-                errorMessages={errorMessages}
-            />
-        </div>
+const GiftForm = ({ gifts, errors, setGifts, formIndex }) => {
+  const errorMessages = {
+    name_wrong_character:
+      '경품명은 특수문자를 제외한 영한문자 그리고 숫자만 가능합니다.',
+    no_file: '파일을 업로드 해주세요',
+    invalid_file: '1mb 이하의 jpg, png 파일만 업로드 가능합니다.',
+  };
+  const onGiftFileChange = (e) => {
+    setGifts(
+      gifts.map((gift, index) => {
+        if (index === formIndex && e.target.files.length > 0)
+          return { ...gift, giftFile: e.target.files[0] };
+        return gift;
+      })
     );
-}
+  };
+  const onGiftNameChange = (e) => {
+    setGifts(
+      gifts.map((gift, index) => {
+        if (index === formIndex) return { ...gift, giftName: e.target.value };
+        return gift;
+      })
+    );
+  };
+  return (
+    <div className='GiftForm'>
+      <div className='form_start'>
+        <p className='form_label gift_name'>경품 이름</p>
+        <p className='form_indicator'>경품 {formIndex + 1}</p>
+      </div>
+      <TextInput
+        type={'gift_name'}
+        defaultValue={gifts[formIndex].giftName}
+        onChange={onGiftNameChange}
+        hasError={errors.name !== 'none'}
+      />
+      <ErrorMessage
+        type={'gift_name'}
+        errorType={errors.name}
+        errorMessages={errorMessages}
+      />
+      <p className='form_label gift_file'>경품 파일</p>
+      <FileUpload
+        fileIndex={formIndex}
+        fileName={gifts[formIndex].giftFile.name}
+        hasError={errors.file !== 'none'}
+        onGiftFileChange={onGiftFileChange}
+        accept={'image/png, image/jpeg'}
+      />
 
-export default GiftForm 
+      <ErrorMessage
+        type={'gift_file'}
+        errorType={errors.file}
+        errorMessages={errorMessages}
+      />
+    </div>
+  );
+};
+
+export default GiftForm;
--- src/app/components/SettingGift.js
+++ src/app/components/SettingGift.js
@@ -1,123 +1,111 @@
-import { useEffect, useState, useContext } from "react";
-import "./SettingGift.css";
-import GiftForm from "./GiftForm";
-import { drawDataContext } from "../drawDataContext";
-import Button from "./ui/Button";
-const SettingGift = ({ moveToPreviousStep,doDraw,moveToNextStep }) => {
-    const {drawData,setDrawData} = useContext(drawDataContext);
-    const [giftDataArray,setGiftData] = useState([...Array(drawData.winnerCount)].map(
-        (_, index) => { return { id : index, giftName : "", giftFile : new File([],"파일 이름")} }
-    ));
-    const [errorDataArray,setErrorData] = useState(giftDataArray.map(() => { return {nameErrorStatus : "none", fileErrorStatus : "none"}}));    
-    const isValidImageFilename = (filename) => {
-        const maxNonNamePartLength = 4;
-        const filenameValidationRegex = /\.JPG$|\.jpg$|\.jpeg$|\.JPEG$|\.png$|\.PNG$/
-        if (filename.length < maxNonNamePartLength)
-        {
-            return false;
-        }
-        if (filenameValidationRegex.test(filename) === false)
-        {
-            return false;
-        }
-        return true;
+import { useEffect, useState, useContext } from 'react';
+import './SettingGift.css';
+import GiftForm from './GiftForm';
+import { drawDataContext } from '../drawDataContext';
+import Button from './ui/Button';
+const SettingGift = ({ moveToPreviousStep, doDraw, moveToNextStep }) => {
+  const { drawData, setDrawData } = useContext(drawDataContext);
+  const [gifts, setGifts] = useState(
+    Array.from({ length: drawData.winnerCount }).map((_, index) => {
+      return { id: index, giftName: '', giftFile: new File([], '파일 이름') };
+    })
+  );
+  const [errorsArray, setErrorsArray] = useState(
+    Array.from({ length: drawData.winnerCount }).map(() => {
+      return { name: 'none', file: 'none' };
+    })
+  );
+  const isValidImageFilename = (filename) => {
+    const maxNonNamePartLength = 4;
+    const filenameValidationRegex =
+      /\.JPG$|\.jpg$|\.jpeg$|\.JPEG$|\.png$|\.PNG$/;
+    if (filename.length < maxNonNamePartLength) {
+      return false;
     }
-    const isValidGiftName = (giftName) => {
-        const nameValidationRegex = /^([a-zA-Zㄱ-ㅎ가-힣 0-9]{1,})$/;
-        if (nameValidationRegex.test(giftName) === false)
-        {
-            return false;
-        }
-        return true;
+    if (filenameValidationRegex.test(filename) === false) {
+      return false;
     }
-    const checkFormError = (formData) => {
-        const errorData = {
-            isErrorOccured : false,
-            nameErrorStatus : "none",
-            fileErrorStatus : "none"
+    return true;
+  };
+  const isValidGiftName = (giftName) => {
+    const nameValidationRegex = /^([a-zA-Zㄱ-ㅎ가-힣 0-9]{1,})$/;
+    if (nameValidationRegex.test(giftName) === false) {
+      return false;
+    }
+    return true;
+  };
+  const checkFormError = (formData, mode = 'no-gift') => {
+    const nowErrors = {
+      name: 'none',
+      file: 'none',
+    };
+    if (isValidGiftName(formData.giftName) === false) {
+      nowErrors.name = 'name_wrong_character';
+    }
+    if (isValidImageFilename(formData.giftFile.name) === false) {
+      nowErrors.file = 'invalid_file';
+    }
+    if (formData.giftFile.size > 1000000) {
+      nowErrors.file = 'invalid_file';
+    }
+    if (
+      formData.giftFile.size === 0 &&
+      formData.giftFile.name === '파일 이름'
+    ) {
+      nowErrors.file = 'no_file';
+    }
+    return nowErrors;
+  };
+  const onClickPreviousButton = () => {
+    setDrawData({ ...drawData, giftArray: [] });
+    moveToPreviousStep();
+  };
 
-        }
-        if (isValidGiftName(formData.giftName) === false)
-        {
-            errorData.isErrorOccured = true;
-            errorData.nameErrorStatus = "name_wrong_character";
-        }
-        if (isValidImageFilename(formData.giftFile.name) === false)
-        {
-            errorData.isErrorOccured = true;
-            errorData.fileErrorStatus = "invalid_file";
-        }
-        if (formData.giftFile.size > 1000000)
-        {
-            errorData.isErrorOccured = true;
-            errorData.fileErrorStatus = "invalid_file";
-        }
-        if (formData.giftFile.size === 0 && formData.giftFile.name === "파일 이름")
-        {
-            errorData.isErrorOccured = true;
-            errorData.fileErrorStatus = "no_file";
-        }
-        return errorData;
-    }
-    const onClickPreviousButton = () => {
-        setDrawData({...drawData,giftArray:[]})
-        moveToPreviousStep();
-    }
-    
-    const onClickDrawButton = () => {
-        let isErrorOccured = false;
-        setErrorData(errorDataArray.map((errorData, index)=> {
-            const errorDataToUpdate = checkFormError(giftDataArray[index]);
-            // 지금까지 에러가 발생하지 않았다면, 현재 에러 상태 확인해서 업데이트
-            if (isErrorOccured === false)
-            {
-                isErrorOccured = errorDataToUpdate.isErrorOccured;
-            }
-            return { 
-                fileErrorStatus: errorDataToUpdate.fileErrorStatus,
-                nameErrorStatus: errorDataToUpdate.nameErrorStatus 
-            };
-        }))
-        if (isErrorOccured === true)
-        {
-            return;
-        }
-        setDrawData({ ...drawData, giftArray: giftDataArray });
-    }
-    useEffect(() => {
-        if (drawData.giftArray.length > 0)
-        {
-            doDraw(false);
-            moveToNextStep();
-        }
-    }
-        ,[drawData,doDraw,moveToNextStep]        
-    )
-
-      
-    return ( 
-        <div className = "setting_block SettingGift">
-            <p className="setting_label">추첨설정</p>
-            <p className="step_label">경품 정하기</p>
-            <div className = "form_wrapper">
-                {
-                    giftDataArray.map( (giftData,index) => {
-                        return <GiftForm 
-                            key={index}
-                            giftDataArray = {giftDataArray}
-                            setGiftData = {setGiftData}
-                            nowIndex = {giftData.id}
-                            errorData={errorDataArray[index]}
-                        />
-                    })
-                }
-            </div>
-            <div className ="button_wrapper">
-                <Button type={'backward_button'} onClick={onClickPreviousButton}>이전</Button>
-                <Button type={'forward_button'} onClick={onClickDrawButton}>추첨하기</Button>
-            </div>
-        </div>
+  const onClickDrawButton = () => {
+    const nowErrorsArray = gifts.map((gift) => checkFormError(gift));
+    const isErrorOccured = nowErrorsArray.some((errors) =>
+      Object.values(errors).some((error) => error != 'none')
     );
-}
+    setErrorsArray(nowErrorsArray);
+    if (isErrorOccured) {
+      return;
+    }
+    setDrawData({ ...drawData, giftArray: gifts });
+  };
+  useEffect(() => {
+    if (drawData.giftArray.length > 0) {
+      doDraw(false);
+      moveToNextStep();
+    }
+  }, [drawData, doDraw, moveToNextStep]);
 
-export default SettingGift
+  return (
+    <div className='setting_block SettingGift'>
+      <p className='setting_label'>추첨설정</p>
+      <p className='step_label'>경품 정하기</p>
+      <div className='form_wrapper'>
+        {gifts.map((gift, index) => {
+          return (
+            <GiftForm
+              key={index}
+              gifts={gifts}
+              setGifts={setGifts}
+              formIndex={gift.id}
+              errors={errorsArray[index]}
+            />
+          );
+        })}
+      </div>
+      <div className='button_wrapper'>
+        <Button type={'backward_button'} onClick={onClickPreviousButton}>
+          이전
+        </Button>
+        <Button type={'forward_button'} onClick={onClickDrawButton}>
+          추첨하기
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default SettingGift;
