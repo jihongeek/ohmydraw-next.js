fbcee960704e3dfcef04badcd4778ee6ce14f089
jihongeek <jihongeek@kakao.com>
2025-02-20 13:32:14 +0900

    Chore: 테스트 관련 의존성 추가 및 vitest 설정

--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-
+import { loadEnvConfig } from '@next/env';
+loadEnvConfig(process.cwd());
 export default defineConfig({
   plugins: [react()],
   test: {
