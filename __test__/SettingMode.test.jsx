4c12823fc10962b18f19e8dd96cf0800b75a6fe9
jihongeek <jihongeek@kakao.com>
2025-03-18 11:00:12 +0900

    Test: oAuth 적용으로 인한 비밀번호 관련 테스트 코드 삭제

--- __test__/SettingMode.test.jsx
+++ __test__/SettingMode.test.jsx
@@ -5,7 +5,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { server } from '../mocks/node';
 import { http, HttpResponse } from 'msw';
-it('단순추첨 라디오 버튼을 클릭시 비밀번호 인풋 표시하지 않기', async () => {
+it('단순추첨 라디오 버튼을 클릭시 로그인 버튼 표시하지 말기', async () => {
   const { unmount } = render(
     <drawDataContext.Provider
       value={{ drawData: { mode: 'gift', sendKey: '' } }}
@@ -16,10 +16,10 @@ it('단순추첨 라디오 버튼을 클릭시 비밀번호 인풋 표시하지 
   const user = userEvent.setup();
   const [_, NoGiftRadioButton] = screen.getAllByRole('radio');
   await user.click(NoGiftRadioButton);
-  expect(screen.queryByText('비밀번호')).toBeNull();
+  expect(screen.queryByText('Google로 로그인')).toBeNull();
   unmount();
 });
-it('경품추첨 라디오 버튼을 클릭시 비밀번호 인풋 표시하기', async () => {
+it('경품추첨 라디오 버튼을 클릭시 로그인 버튼 표시하기', async () => {
   const { unmount } = render(
     <drawDataContext.Provider
       value={{ drawData: { mode: 'gift', sendKey: '' } }}
@@ -30,66 +30,6 @@ it('경품추첨 라디오 버튼을 클릭시 비밀번호 인풋 표시하기'
   const user = userEvent.setup();
   const [GiftRadioButton, _] = screen.getAllByRole('radio');
   await user.click(GiftRadioButton);
-  expect(screen.getAllByText('비밀번호')).toBeDefined();
-  unmount();
-});
-
-it('비밀번호를 입력하지 않았을 경우, 알맞은 에러 메시지 출력하기', async () => {
-  const { unmount } = render(
-    <drawDataContext.Provider
-      value={{ drawData: { mode: 'gift', sendKey: '' } }}
-    >
-      <SettingMode />
-    </drawDataContext.Provider>
-  );
-  const user = userEvent.setup();
-  await user.click(screen.getByText('다음'));
-  expect(screen.getAllByText('비밀번호를 입력해 주세요.')).toBeDefined();
-  unmount();
-});
-it('비밀번호를 틀렸을 경우, 알맞은 에러 메시지 출력하기', async () => {
-  const { unmount } = render(
-    <drawDataContext.Provider
-      value={{ drawData: { mode: 'gift', sendKey: '' } }}
-    >
-      <SettingMode />
-    </drawDataContext.Provider>
-  );
-  server.use(
-    http.post(`${process.env.NEXT_PUBLIC_BASE_URL}/sendkey/validate`, () => {
-      return HttpResponse.json(JSON.stringify({ isValid: false }), {
-        status: 200,
-      });
-    })
-  );
-  const user = userEvent.setup();
-  const WRONG_SEND_KEY = `${process.env.SEND_KEY}`;
-  await user.type(screen.getByLabelText('비밀번호'), WRONG_SEND_KEY);
-  await user.click(screen.getByText('다음'));
-  expect(screen.getAllByText('올바르지 않은 비밀번호입니다.')).toBeDefined();
-  unmount();
-});
-it('비밀번호를 확인할 수 없을 경우, 알맞은 에러 메시지 출력하기', async () => {
-  const { unmount } = render(
-    <drawDataContext.Provider
-      value={{ drawData: { mode: 'gift', sendKey: '' } }}
-    >
-      <SettingMode />
-    </drawDataContext.Provider>
-  );
-  server.use(
-    http.post(`${process.env.NEXT_PUBLIC_BASE_URL}/sendkey/validate`, () => {
-      return HttpResponse.json('', {
-        status: 500,
-      });
-    })
-  );
-  const user = userEvent.setup();
-  const WRONG_SEND_KEY = `${process.env.SEND_KEY}`;
-  await user.type(screen.getByLabelText('비밀번호'), WRONG_SEND_KEY);
-  await user.click(screen.getByText('다음'));
-  expect(
-    screen.getAllByText('현재 비밀번호를 확인할 수 없습니다.')
-  ).toBeDefined();
+  expect(screen.getAllByText('Google로 로그인')).toBeDefined();
   unmount();
 });
